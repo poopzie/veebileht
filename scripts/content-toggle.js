bb2ec9f7aa8d41384a77c09bf7c92bef11f385a2
AloMartinP <73949637+AloMartinP@users.noreply.github.com>
2021-11-27 16:47:02 +0200

    Uus proov

---
+++ scripts/content-toggle.js
@@ -0,0 +1,62 @@
+//Video tutoorial  https://www.youtube.com/watch?v=KcdBOoK3Pfw&ab_channel=DevEd
+const contentSlide = document.querySelector('.content-slider-images');
+const contentImages = document.querySelectorAll('.content-slider-images img');
+
+//Defineerime nupud
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Kontrollime mis kell on ja selle järgi valime, mis pildi kuvame
+$(document).ready(function() {
+    function dateTime() {
+        var ndate = new Date();
+        var hours = ndate.getHours();
+    setInterval(dateTime, 1000);
+    }
+});
+
+Number.prototype.leadingZeroes = function(len) {
+    return (new Array(len).fill('0', 0).join('') + this).slice(-Math.abs(len));
+}
+
+//Counter checks daytime
+var d = new Date()
+let counter = 1
+if (d.getHours() < 12){
+    counter = 1;
+}else if(d.getHours()<18){
+    counter = 2;
+}else{
+    counter = 3;
+}
+
+const size = contentImages[0].clientWidth;
+
+contentSlide.style.transform = 'translateX('+ ( -size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener('click', () =>{
+    if (counter >= contentImages.length -1) return;
+    contentSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    contentSlide.style.transform = 'translateX(' + ( -size * counter) + 'px)';
+});
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    contentSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    contentSlide.style.transform = 'translateX(' + ( -size * counter) + 'px)';
+});
+contentSlide.addEventListener('transitionend', ()=>{
+    if (contentImages[counter].id === "lastClone"){
+        contentSlide.style.transition = "none"
+        counter = contentImages.length -2;
+        contentSlide.style.transform = 'translateX(' + ( -size * counter) + 'px)';
+    }
+    if (contentImages[counter].id === "firstClone"){
+        contentSlide.style.transition = "none"
+        counter = contentImages.length-counter;
+        contentSlide.style.transform = 'translateX(' + ( -size * counter) + 'px)';
+    }
+});
